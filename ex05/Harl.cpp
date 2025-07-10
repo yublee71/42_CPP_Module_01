5d258f3b968a89f3518bc9ba664d9cbba5f3a049
Yubeen Lee <lybin71@naver.com>
2025-07-10 18:00:17 +0100

    style: modifying input and output messages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,20 +6,22 @@
 void Harl::complain(std::string level) {
   void (Harl::*function_ptrs[])(void) = {&Harl::debug, &Harl::info,
                                          &Harl::warning, &Harl::error};
-  std::string levels[] = {"debug", "info", "warning", "error"};
+  std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
   for (int i = 0; i < 4; i++) {
     if (levels[i] == level) {
       (this->*function_ptrs[i])();
       return;
     }
   }
-  std::cout << "No level matched." << std::endl;
+  std::cout << "[ Probably complaining about insignificant problems ]"
+            << std::endl;
 }
 
-void Harl::debug(void) { std::cout << "[DEBUG]" << std::endl; }
+void Harl::debug(void) { std::cout << "[ DEBUG ]" << std::endl; }
 
-void Harl::info(void) { std::cout << "[INFO]" << std::endl; }
+void Harl::info(void) { std::cout << "[ INFO ]" << std::endl; }
 
-void Harl::warning(void) { std::cout << "[WARNING]" << std::endl; }
+void Harl::warning(void) { std::cout << "[ WARNING ]" << std::endl; }
 
-void Harl::error(void) { std::cout << "[ERROR]" << std::endl; }
+void Harl::error(void) { std::cout << "[ ERROR ]" << std::endl; }
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -3,10 +3,10 @@
 int main(void) {
   Harl harl;
 
-  harl.complain("debug");
-  harl.complain("info");
-  harl.complain("warning");
-  harl.complain("error");
+  harl.complain("DEBUG");
+  harl.complain("INFO");
+  harl.complain("WARNING");
+  harl.complain("ERROR");
   harl.complain("");
-  harl.complain("Error");
+  harl.complain("eRROR");
 }
